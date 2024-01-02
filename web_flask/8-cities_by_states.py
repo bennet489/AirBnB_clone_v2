aa614858b00ce29ef895f1923f19e38943d465c6
Bennet489 <bennetasare@outlook.com>
2024-01-02 05:15:56 -0800

    webbly

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-
+"""Cities by states
+"""
+from flask import Flask, render_template
+from os import environ
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def states_list():
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+environ['FLASK_ENV'] = 'development'
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def states_list_teardown(self):
+    """ Ensures SQLAlchemy session opened to serve dynamic content for HTML
+    templates is closed after serving.
+    """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Requests list of `State`s ordered by name, which populates HTML
+    template served to '/cities_by_states'.
+    """
+    return render_template('8-cities_by_states.html',
+                           states=storage.all(State))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
