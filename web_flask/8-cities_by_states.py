e5863de6bdbed83cdf5389c68065eabc7472ea73
Bennet489 <bennetasare@outlook.com>
2024-01-02 05:30:14 -0800

    butterfly doors

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,56 +1,31 @@
 #!/usr/bin/python3
-"""starts a flask web application
+"""Cities by states
 """
-
 from flask import Flask, render_template
-
+from os import environ
 from models import storage
 from models.state import State
-from os import environ
 
 app = Flask(__name__)
+environ['FLASK_ENV'] = 'development'
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """removes the current SQLAlchemy session
+def states_list_teardown(self):
+    """ Ensures SQLAlchemy session opened to serve dynamic content for HTML
+    templates is closed after serving.
     """
-
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """displays a HTML page with a list of states
-
-    Return:
-        return an html template containing a list of states
-    """
-
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-
-    return render_template('7-states_list.html', states=states)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_list():
-    """displays a HTML page with a list of cities by states
-
-    Return:
-        return an html template rendering a list of cities
+def cities_by_states():
+    """ Requests list of `State`s ordered by name, which populates HTML
+    template served to '/cities_by_states'.
     """
-
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
     return render_template('8-cities_by_states.html',
-                           states=st_ct,
-                           h_1="States")
+                           states=storage.all(State))
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
